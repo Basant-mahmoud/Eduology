7bd35ed0dca220c5ca66e2e48de9e89c921107d2
MuhammedCoding <eng.mhd.ahmed@gmail.com>
2024-05-06 01:53:26 +0300

    implment-demo-class

---
+++ Eduology.Infrastructure/TestClass.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Eduology.Infrastructure
+{
+    internal class TestClass
+    {
+        public int Id { get; set; }
+    }
+}
