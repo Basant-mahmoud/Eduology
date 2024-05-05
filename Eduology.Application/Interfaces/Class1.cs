a8ef99db580c95970a36dbed4224ae847e87f3ce
Basant Mahmoud <103046859+basanttt@users.noreply.github.com>
2024-05-05 14:22:43 +0300

    frist commit

--- Eduology.Application/Interfaces/Class1.cs
+++ Eduology.Application/Interfaces/Class1.cs
@@ -4,9 +4,9 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace Eduology.Infrastructure.Persistence
+namespace Eduology.Application.Interfaces
 {
-    internal class EduologyContext
+    internal class Class1
     {
     }
 }
