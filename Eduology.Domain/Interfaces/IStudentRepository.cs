018f4cd5adc31716ef84c593823b5b918e0cf4f4
Walaa Hassan <walaa.elbishlawy@gmail.com>
2024-06-17 13:24:17 +0300

    DeleteStudent API

---
+++ Eduology.Domain/Interfaces/IStudentRepository.cs
@@ -0,0 +1,17 @@
+﻿using Eduology.Domain.DTO;
+using Eduology.Domain.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Eduology.Domain.Interfaces
+{
+    public interface IStudentRepository
+    {
+        Task<UserDto> GetStudentByIdAsync(string userId);
+        Task<IEnumerable<UserDto>> GetAllStudentsAsync();
+        Task<bool> DeleteAsync(string studentId);
+    }
+}
