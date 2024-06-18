5d0498fc99f9608d718c60da094890bba6e7d806
Aliaa Mohammed <110140230+Aliaa-Elbishlawy@users.noreply.github.com>
2024-06-18 18:28:02 +0300

    #3 update

---
+++ Eduology.Infrastructure/Migrations/20240618152734_update_database.cs
@@ -0,0 +1,102 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Eduology.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class update_database : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_StudentCourse_AspNetUsers_StudentId",
+                table: "StudentCourse");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_StudentCourse_Courses_CourseId",
+                table: "StudentCourse");
+
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_StudentCourse",
+                table: "StudentCourse");
+
+            migrationBuilder.RenameTable(
+                name: "StudentCourse",
+                newName: "StudentCourses");
+
+            migrationBuilder.RenameIndex(
+                name: "IX_StudentCourse_CourseId",
+                table: "StudentCourses",
+                newName: "IX_StudentCourses_CourseId");
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_StudentCourses",
+                table: "StudentCourses",
+                columns: new[] { "StudentId", "CourseId" });
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_StudentCourses_AspNetUsers_StudentId",
+                table: "StudentCourses",
+                column: "StudentId",
+                principalTable: "AspNetUsers",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_StudentCourses_Courses_CourseId",
+                table: "StudentCourses",
+                column: "CourseId",
+                principalTable: "Courses",
+                principalColumn: "CourseId",
+                onDelete: ReferentialAction.Cascade);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_StudentCourses_AspNetUsers_StudentId",
+                table: "StudentCourses");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_StudentCourses_Courses_CourseId",
+                table: "StudentCourses");
+
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_StudentCourses",
+                table: "StudentCourses");
+
+            migrationBuilder.RenameTable(
+                name: "StudentCourses",
+                newName: "StudentCourse");
+
+            migrationBuilder.RenameIndex(
+                name: "IX_StudentCourses_CourseId",
+                table: "StudentCourse",
+                newName: "IX_StudentCourse_CourseId");
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_StudentCourse",
+                table: "StudentCourse",
+                columns: new[] { "StudentId", "CourseId" });
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_StudentCourse_AspNetUsers_StudentId",
+                table: "StudentCourse",
+                column: "StudentId",
+                principalTable: "AspNetUsers",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_StudentCourse_Courses_CourseId",
+                table: "StudentCourse",
+                column: "CourseId",
+                principalTable: "Courses",
+                principalColumn: "CourseId",
+                onDelete: ReferentialAction.Cascade);
+        }
+    }
+}
