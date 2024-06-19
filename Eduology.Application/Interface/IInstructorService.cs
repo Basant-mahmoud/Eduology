32df3f1c740fd61c1e622ee50fe51de6a51e1cfd
Walaa Hassan <walaa.elbishlawy@gmail.com>
2024-06-19 14:40:48 +0300

    Services

---
+++ Eduology.Application/Interface/IInstructorService.cs
@@ -0,0 +1,19 @@
+﻿using Eduology.Domain.DTO;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Eduology.Application.Interface
+{
+    public interface IInstructorService
+    {
+        Task<IEnumerable<UserDto>> GetAllInstructorsAsync();
+        Task<UserDto> GetInstructorByIdAsync(string id);
+        Task<UserDto> GetInstructorByNameAsync(string name);
+        Task<UserDto> GetInstructorByUserNameAsync(string userName);
+        Task<bool> DeleteInstructorAsync(string id);
+        Task<bool> UpdateInstructorAsync(string id, UserDto updateInstructorDto);
+    }
+}
