f801bc49ab3e9460894bdc7bd7aac107bab51ae0
Aliaa Mohammed <110140230+Aliaa-Elbishlawy@users.noreply.github.com>
2024-07-04 20:21:45 +0300

    payment

---
+++ Eduology.Infrastructure/External Services/payment/Paymob/PaymentModels/MerchantDetails.cs
@@ -0,0 +1,42 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Eduology.Infrastructure.External_Services.payment.Paymob.PaymentModels
+{
+    public class MerchantDetails
+    {
+        [JsonProperty("id")]
+        public int Id { get; set; }
+
+        [JsonProperty("created_at")]
+        public DateTime CreatedAt { get; set; }
+
+        [JsonProperty("phones")]
+        public List<string> Phones { get; set; }
+
+        [JsonProperty("company_emails")]
+        public List<string> CompanyEmails { get; set; }
+
+        [JsonProperty("company_name")]
+        public string CompanyName { get; set; }
+
+        [JsonProperty("state")]
+        public string State { get; set; }
+
+        [JsonProperty("country")]
+        public string Country { get; set; }
+        [JsonProperty("city")]
+        public string City { get; set; }
+
+        [JsonProperty("postal_code")]
+        public string PostalCode { get; set; }
+
+        [JsonProperty("street")]
+        public string Street { get; set; }
+    }
+
+}
