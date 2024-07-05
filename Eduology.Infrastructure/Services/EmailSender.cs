baf902602725c6784c49b230728b9f72ed8193cd
Aliaa Mohammed <110140230+Aliaa-Elbishlawy@users.noreply.github.com>
2024-07-06 00:04:49 +0300

    email

--- Eduology.Infrastructure/Services/EmailSender.cs
+++ Eduology.Infrastructure/Services/EmailSender.cs
@@ -10,16 +10,16 @@ namespace Eduology.Infrastructure.Services
     {
         public Task SendEmailAsync(string email, string subject, string message)
         {
-            var mail = "Mohamedhamdyy55@outlook.com";
-            var password = "01157417179Mh"; // Corrected variable name
+            var mail = "eduology24@outlook.com";
+            var password = "eduology@2024"; 
              var client = new SmtpClient("smtp-mail.outlook.com", 587)
             {
                 EnableSsl = true,
-                Credentials = new NetworkCredential(mail, password) // Corrected variable name and object initialization
+                Credentials = new NetworkCredential(mail, password)
             };
 
             var mailMessage = new MailMessage(from: mail, to: email, subject: subject, body: message);
-            return client.SendMailAsync(mailMessage); // Corrected method name and object initialization
+            return client.SendMailAsync(mailMessage); 
         }
     }
 }
